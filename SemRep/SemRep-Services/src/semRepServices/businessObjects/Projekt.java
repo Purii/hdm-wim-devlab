bb19cb666c5d0d69287c420b14bc0d2a60d54802
mateos-alliaj <mateos.alliaj@gmail.com>
2017-06-06 20:37:08 +0200

    adding businessObject 'Projekt'

---
+++ SemRep/SemRep-Services/src/semRepServices/businessObjects/Projekt.java
@@ -0,0 +1,16 @@
+package semRepServices.businessObjects;
+
+public class Projekt {
+	
+	public String projektID;
+	public String projektName;
+	public String projekt_gehoert_zu_Unternehmen;
+	public String projekt_gehoert_zu_Abteilung;
+	public String projekt_hat_Projektmitglied;
+	public String projekt_hat_Dokument;
+	
+	public Projekt(String projektID, String projektName){
+		
+	}
+
+}
