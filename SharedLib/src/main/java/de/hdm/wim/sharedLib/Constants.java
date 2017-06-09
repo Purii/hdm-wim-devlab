a556235c3820191132dacf6dd119f0b7db36f418
Grasniqi <gezim.grasniqi@gmail.com>
2017-06-09 14:42:53 +0200

    Create Constants.java
    Updated class EventType and Topic

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
@@ -41,11 +41,13 @@ public class Constants {
 	 * Format: "my-test-topic-x"
      */
     public static class Topic {
-		public static final String TOPIC_1 = "topic-1";
-		public static final String TOPIC_2 = "topic-2";
-		public static final String TOPIC_3 = "topic-3";
-		public static final String TOPIC_4 = "topic-4";
-		public static final String TOPIC_5 = "topic-5";
+		public static final String FEEDBACK_GUI = "feedback-gui";
+		public static final String INSIGHTS = "INSIGHTS";
+		public static final String OFFERS = "offers";
+		public static final String SESSIONINSIGHTS = "sessioninsights";
+		public static final String RICHTOKEN = "richtoken";
+	    public static final String TOKEN = "token";
+
 
 		public static final String TOPIC_EVENT 		= "topic-event";
 		public static final String TOPIC_SEM_REP 	= "topic-semRep";
@@ -56,11 +58,11 @@ public class Constants {
 
 		// for message generator only
 		public static final List<String> list = Arrays.asList(
-			TOPIC_1,
-			TOPIC_2,
-			TOPIC_3,
-			TOPIC_4,
-			TOPIC_5
+			FEEDBACK_GUI,
+			INSIGHTS,
+			TOKEN,
+			RICHTOKEN,
+			SESSIONINSIGHTS
 		);
     }
 
@@ -68,21 +70,23 @@ public class Constants {
 	 * Event types. -> einheitliche Attribute (siehe READEME)
 	 */
 	public static class EventType {
-		public static final String ACTION 	= "action";
-		public static final String REQUEST 	= "request";
-		public static final String USER 	= "user";
-		public static final String FEEDBACK	= "feedback";
-		public static final String TIME		= "time";
-		public static final String DATE		= "date";
+		public static final String STAYALIVE 	= "stayalive";
+		public static final String INSIGHT  	= "insight";
+		public static final String GOOGLEOFFER 	= "googleoffer";
+		public static final String OFFER	    = "offer";
+		public static final String TOKEN		= "token";
+		public static final String RICHTOKEN	= "richtoken";
+		public static final String FEEDBACK		= "feedback";
 
 		// for message generator only
 		public static final List<String> list = Arrays.asList(
-			ACTION,
-			REQUEST,
-			USER,
-			FEEDBACK,
-			TIME,
-			DATE
+			STAYALIVE,
+			INSIGHT,
+			GOOGLEOFFER,
+			OFFER,
+			TOKEN,
+			RICHTOKEN,
+			FEEDBACK
 		);
 	}
 
