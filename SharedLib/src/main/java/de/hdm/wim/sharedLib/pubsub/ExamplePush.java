4de1abd5c189b657a6a4aa0b00b49358cbee16da
itsBen <benedikt.zneb@gmail.com>
2017-06-13 11:42:10 +0200

    example for push, NOT WORKING YET!

---
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/ExamplePush.java
@@ -0,0 +1,25 @@
+package de.hdm.wim.sharedLib.pubsub;
+
+import com.google.pubsub.v1.Topic;
+import de.hdm.wim.sharedLib.Constants.PubSub;
+import de.hdm.wim.sharedLib.Constants.PubSub.SubscriptionType;
+import de.hdm.wim.sharedLib.pubsub.helper.SubscriptionHelper;
+import de.hdm.wim.sharedLib.pubsub.helper.TopicHelper;
+
+/**
+ * Created by ben on 11/06/2017.
+ * Erstellt eine push subscription
+ */
+public class ExamplePush {
+
+	public static void main(String[] args) throws Exception {
+
+		// create the topic
+		TopicHelper th = new TopicHelper();
+		Topic topic    = th.createTopicIfNotExists(PubSub.Topic.TOPIC_1);
+
+		// create the subscription
+		SubscriptionHelper sh = new SubscriptionHelper(false);
+		sh.Subscribe(SubscriptionType.PUSH, topic.getNameAsTopicName());
+	}
+}
