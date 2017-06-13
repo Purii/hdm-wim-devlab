74b9063d4a7a66ba12a754002048f5e5499fd14c
Patrick Puritscher <blackout289@gmail.com>
2017-06-13 11:45:06 +0200

    added default methods to eventclass

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/events/Event.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/events/Event.java
@@ -6,8 +6,13 @@ import de.hdm.wim.sharedLib.pubsub.helper.PublishHelper;
 
 /**
  * To be implemented by specific Event-Classes
+ * @Todo Attributes
  */
 public abstract class Event {
+    private String eventId;
+    private String publishTime;
+    private String data;
+
     protected abstract Message getAsMessage();
 
     /**
@@ -16,6 +21,34 @@ public abstract class Event {
      * @throws Exception
      */
     public void publishToPubSub() throws Exception {
-       
+
+    }
+
+    public Event(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setMessageId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(String publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
     }
 }
