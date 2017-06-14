0bc771cebca4cdff4c7645997b39b09dafdde6b0
itsBen <benedikt.zneb@gmail.com>
2017-06-14 21:47:44 +0200

    updated example

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/ExamplePull.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/ExamplePull.java
@@ -1,22 +1,31 @@
 package de.hdm.wim.sharedLib.pubsub;
 
+import com.google.pubsub.v1.Subscription;
 import de.hdm.wim.sharedLib.Constants.PubSub;
+import de.hdm.wim.sharedLib.Constants.PubSub.Config;
 import de.hdm.wim.sharedLib.Constants.PubSub.SubscriptionType;
 import de.hdm.wim.sharedLib.pubsub.helper.SubscriptionHelper;
 
 /**
  * Created by ben on 11/06/2017.
- * Erstellt eine pull subscription
  */
 public class ExamplePull {
 
 	public static void main(String[] args) throws Exception {
 
-		// create the subscription
-		SubscriptionHelper sh = new SubscriptionHelper(true);
+		// init a SubscriptionHelper to use for prod environment, without REST and for the given project
+		SubscriptionHelper sh 		= new SubscriptionHelper(false, false, Config.APP_ID);
 
-		//TODO: suffix/prefix to create multiple subscriptions for one topic
-		//TODO: receiver to handle message
-		sh.Subscribe(SubscriptionType.PULL, PubSub.Topic.TOPIC_1);
+		/**
+		 * this will create a subscription with id: "subscription-pull-topic-1-test1"
+		 * if the subscription already exists, we will use it
+		 *
+		 * make sure to create the subscription(s) BEFORE publishing
+		 * this example will work because the subscriptions already exist
+		 */
+		Subscription subscription 	= sh.CreateSubscription(SubscriptionType.PULL, PubSub.Topic.TOPIC_1, "test1");
+
+		// subscribe using the existing subscription
+		sh.Subscribe(subscription);
 	}
 }
