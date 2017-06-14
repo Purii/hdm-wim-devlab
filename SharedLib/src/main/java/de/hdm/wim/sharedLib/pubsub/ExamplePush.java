39562cb92260475d5a55ebaa567ee719f0c9bd87
itsBen <benedikt.zneb@gmail.com>
2017-06-14 21:52:32 +0200

    updated push example, still WIP

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/ExamplePush.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/ExamplePush.java
@@ -1,19 +1,22 @@
 package de.hdm.wim.sharedLib.pubsub;
 
+import com.google.pubsub.v1.Subscription;
 import de.hdm.wim.sharedLib.Constants.PubSub;
+import de.hdm.wim.sharedLib.Constants.PubSub.Config;
 import de.hdm.wim.sharedLib.Constants.PubSub.SubscriptionType;
 import de.hdm.wim.sharedLib.pubsub.helper.SubscriptionHelper;
 
 /**
  * Created by ben on 11/06/2017.
- * Erstellt eine push subscription
  */
 public class ExamplePush {
 
 	public static void main(String[] args) throws Exception {
 
 		// create the subscription
-		SubscriptionHelper sh = new SubscriptionHelper(true);
-		sh.Subscribe(SubscriptionType.PUSH, PubSub.Topic.TOPIC_1);
+		SubscriptionHelper sh 		= new SubscriptionHelper(false, false, Config.APP_ID);
+		Subscription subscription 	= sh.CreateSubscription(SubscriptionType.PUSH, PubSub.Topic.TOPIC_1, "test1");
+
+		sh.Subscribe(subscription);
 	}
 }
