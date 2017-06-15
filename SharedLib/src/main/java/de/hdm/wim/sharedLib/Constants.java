0bc92cba70e959359421f668d7e36e3a0729543d
Christian Schneider <cschnei1@web.de>
2017-06-15 13:10:11 +0200

    Create Constants.java

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
@@ -43,26 +43,28 @@ public class Constants {
 		 * Format: "my-test-topic-x"
 		 */
 		public static class Topic {
+			// für Testzwecke
 			public static final String TOPIC_1 = "topic-1";
 			public static final String TOPIC_2 = "topic-2";
 			public static final String TOPIC_3 = "topic-3";
 			public static final String TOPIC_4 = "topic-4";
 			public static final String TOPIC_5 = "topic-5";
-
-			public static final String FEEDBACK_GUI 	= "feedback-gui";
-			public static final String INSIGHTS 		= "insights";
-			public static final String OFFERS 			= "offers";
-			public static final String SESSIONINSIGHTS 	= "sessioninsights";
-			public static final String RICHTOKEN 		= "richtoken";
-			public static final String TOKEN 			= "token";
-
-
 			public static final String TOPIC_EVENT 		= "topic-event";
 			public static final String TOPIC_SEM_REP 	= "topic-semRep";
 			public static final String TOPIC_ML 		= "topic-ml";
 			public static final String TOPIC_GUI 		= "topic-gui";
-
 			public static final String PUSH_TEST 		= "topic-push-test-123";
+
+			
+			// für die Produktion
+			public static final String FEEDBACK_GUI 	= "feedback-gui";
+			public static final String INSIGHTS 		= "insights";
+			public static final String OFFERS 			= "offers";
+			public static final String SESSIONINSIGHTS 	= "sessioninsights";
+			public static final String TOKEN 			= "token";
+			public static final String INFORMATION		= "information";
+			
+
 
 			// for message generator only
 			public static final List<String> list = Arrays.asList(
@@ -73,9 +75,10 @@ public class Constants {
 				TOPIC_5,
 				FEEDBACK_GUI,
 				INSIGHTS,
+				OFFERS,
 				TOKEN,
-				RICHTOKEN,
-				SESSIONINSIGHTS
+				SESSIONINSIGHTS,
+				INFORMATION
 			);
 		}
 
@@ -84,6 +87,20 @@ public class Constants {
 		 */
 		public static class EventType {
 			public static final String ACTION 		= "action";
+			public static final String USER_INFO		= "user-information";
+			public static final String DOCUMENT_INFO		= "document-information";
+			public static final String USER_CONTEXT = "user-context";
+			public static final String DOCUMENT_CONTEXT		= "document-context";
+			public static final String DOCUMENT_RELEVANT	= "document-relevant";
+			public static final String DOCUMENT_HIGHLY_RELEVANT	= "document-highly-relevant";
+			public static final String SESSION_START	= "session-start";
+			public static final String SESSION_END	= "session-end";
+			public static final String USER_INACTIVE	= "user-inactive";
+			public static final String USER_LOGIN	= "user-login";
+			public static final String USER_LOGOUT	= "user-logout";
+			public static final String USER_JOINED_SESSION	= "user-joined-session";
+			public static final String USER_LEFT_SESSION	= "user-left-session";
+			public static final String LEARN	= "learning";
 			public static final String REQUEST 		= "request";
 			public static final String USER 		= "user";
 			public static final String TIME			= "time";
