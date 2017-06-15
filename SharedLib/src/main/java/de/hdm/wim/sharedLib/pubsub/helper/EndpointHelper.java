dd2f834d2e67560e06212408ca24fcf1a6cf18f6
itsBen <benedikt.zneb@gmail.com>
2017-06-15 10:08:51 +0200

    init endpointHelper

---
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/pubsub/helper/EndpointHelper.java
@@ -0,0 +1,32 @@
+package de.hdm.wim.sharedLib.pubsub.helper;
+
+import de.hdm.wim.sharedLib.Constants.PubSub.Config;
+import de.hdm.wim.sharedLib.Constants.RequestParameters;
+
+/**
+ * Created by ben on 14/06/2017.
+ */
+public class EndpointHelper {
+
+	public static String GetPushEndpoint(boolean isLocal){
+		String endpoint;
+
+		if(isLocal)
+			endpoint = Config.LOCAL_ADDRESS;
+		else
+			endpoint = Config.APPSPOT_URL;
+
+		return endpoint + Config.PUSH_ENDPOINT + "?" + RequestParameters.SECRET_TOKEN + "=" + Config.SECRET_TOKEN;
+	}
+
+	public static String GetPublishEndpoint(boolean isLocal){
+		String endpoint;
+
+		if(isLocal)
+			endpoint = Config.LOCAL_ADDRESS;
+		else
+			endpoint = Config.APPSPOT_URL;
+
+		return endpoint + Config.PUBLISH_ENDPOINT;
+	}
+}
