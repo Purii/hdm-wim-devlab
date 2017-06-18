333149bb18774ef96ea7d590ceb6e97f28c7402e
itsBen <benedikt.zneb@gmail.com>
2017-06-18 18:37:32 +0200

    added methods to get/set eventsource & eventtype

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/events/Event.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/events/Event.java
@@ -6,22 +6,13 @@ import de.hdm.wim.sharedLib.Constants.PubSub.EventType;
 import de.hdm.wim.sharedLib.testing.Helper;
 import java.util.Hashtable;
 
-/**
- * Created by ben on 3/06/2017.
- */
 public class Event implements IEvent {
 
 	private String data;
 	private String id;
 	private String publishTime;
-	private Hashtable attributes = new Hashtable();
+	private Hashtable<String, String> attributes = new Hashtable<String, String>();
 
-	/**
-	 * Instantiates a new Message.
-	 *
-	 * @param data the data
-	 * @param attributes the attributes
-	 */
 	public Event( String data, Hashtable attributes ){
 		this.data 		= data;
 		this.attributes = attributes;
@@ -31,53 +22,24 @@ public class Event implements IEvent {
 		this.data 		= data;
 	}
 
-	/**
-	 * Instantiates a new Message.
-	 */
 	public Event(){}
 
-	/**
-	 * Gets data.
-	 *
-	 * @return the data
-	 */
 	public String getData() {
 		return data;
 	}
 
-	/**
-	 * Sets data.
-	 *
-	 * @param data the data
-	 */
 	public void setData(String data) {
 		this.data = data;
 	}
 
-	/**
-	 * Gets attributes.
-	 *
-	 * @return the attributes
-	 */
-	public Hashtable getAttributes() {
+	public Hashtable<String, String> getAttributes() {
 		return attributes;
 	}
 
-	/**
-	 * Sets attributes.
-	 *
-	 * @param attributes the attributes
-	 */
 	public void setAttributes(Hashtable attributes) {
 		this.attributes = attributes;
 	}
 
-	/**
-	 * Generate message.
-	 *
-	 * @param data the data
-	 * @return the message
-	 */
 	public static Event generate(String data){
 
 		Helper helper 			= new Helper();
@@ -96,39 +58,35 @@ public class Event implements IEvent {
 		return new Event(data, attributes);
 	}
 
-	/**
-	 * Gets id.
-	 *
-	 * @return the id
-	 */
 	public String getId() {
 		return id;
 	}
 
-	/**
-	 * Sets id.
-	 *
-	 * @param id the id
-	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
-	/**
-	 * Gets publish time.
-	 *
-	 * @return the publish time
-	 */
 	public String getPublishTime() {
 		return publishTime;
 	}
 
-	/**
-	 * Sets publish time.
-	 *
-	 * @param publishTime the publish time
-	 */
 	public void setPublishTime(String publishTime) {
 		this.publishTime = publishTime;
+	}
+
+	public String getEventType(){
+		return attributes.get(AttributeKey.EVENT_TYPE).toString();
+	}
+
+	public void setEventType(String EventType){
+		this.attributes.put(AttributeKey.EVENT_TYPE, EventType);
+	}
+
+	public String getEventSource(){
+		return attributes.get(AttributeKey.EVENT_SOURCE).toString();
+	}
+
+	public void setEventSource(String EventSource){
+		this.attributes.put(AttributeKey.EVENT_SOURCE, EventSource);
 	}
 }
