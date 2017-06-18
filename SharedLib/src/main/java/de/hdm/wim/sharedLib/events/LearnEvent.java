e1bb7ac276604fba60468262cb497cfc2cd73139
itsBen <benedikt.zneb@gmail.com>
2017-06-18 18:38:12 +0200

    init LearnEvent class with example to use

---
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/events/LearnEvent.java
@@ -0,0 +1,114 @@
+package de.hdm.wim.sharedLib.events;
+
+import de.hdm.wim.sharedLib.Constants.PubSub.AttributeKey;
+import de.hdm.wim.sharedLib.Constants.PubSub.EventType;
+import java.util.Hashtable;
+
+/**
+ * Created by ben on 18/06/2017.
+ *
+ * Example:
+ * 	LearnEvent learnEvent = new LearnEvent();
+ *	learnEvent.setData("test");
+ *	learnEvent.setDocumentId("");
+ *	learnEvent.setEventSource(EventSource.MACHINE_LEARNING);
+ *	learnEvent.setProjectId("test project id");
+ *	learnEvent.setUserclick(false);
+ *	learnEvent.setUserId("test user id");
+ *
+ */
+public class LearnEvent implements IEvent {
+
+	private String data;
+	private String id;
+	private String publishTime;
+	private Hashtable attributes = new Hashtable<String, String>();
+
+	// auto set EventType on init
+	public LearnEvent() {
+		this.attributes.put(AttributeKey.EVENT_TYPE, EventType.LEARN);
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPublishTime() {
+		return publishTime;
+	}
+
+	public void setPublishTime(String publishTime) {
+		this.publishTime = publishTime;
+	}
+
+	public String getUserId(){
+		return attributes.get(AttributeKey.USER_ID).toString();
+	}
+
+	public void setUserId(String userId){
+		this.attributes.put(AttributeKey.USER_ID, userId);
+	}
+
+	public String getDocumentId(){
+		return attributes.get(AttributeKey.DOCUMENT_ID).toString();
+	}
+
+	public void setDocumentId(String DocumentId){
+		this.attributes.put(AttributeKey.DOCUMENT_ID, DocumentId);
+	}
+
+	public String getProjectId(){
+		return attributes.get(AttributeKey.PROJECT_ID).toString();
+	}
+
+	public void setProjectId(String ProjectId){
+		this.attributes.put(AttributeKey.PROJECT_ID, ProjectId);
+	}
+
+	public boolean getUserclick(){
+		String val = attributes.get(AttributeKey.USERCLICK).toString();
+		return Boolean.parseBoolean(val);
+	}
+
+	public void setUserclick(boolean Userclick){
+		this.attributes.put(AttributeKey.USERCLICK, Boolean.toString(Userclick));
+	}
+
+	public String getEventType(){
+		return attributes.get(AttributeKey.EVENT_TYPE).toString();
+	}
+
+	/*
+	public void setEventType(String EventType){
+		this.attributes.put(AttributeKey.EVENT_TYPE, EventType);
+	}
+	*/
+
+	public String getEventSource(){
+		return attributes.get(AttributeKey.EVENT_SOURCE).toString();
+	}
+
+	public void setEventSource(String EventSource){
+		this.attributes.put(AttributeKey.EVENT_SOURCE, EventSource);
+	}
+
+	public Hashtable<String, String> getAttributes() {
+		return this.attributes;
+	}
+
+	public void setAttributes(Hashtable<String, String> attributes) {
+		this.attributes = attributes;
+	}
+}
