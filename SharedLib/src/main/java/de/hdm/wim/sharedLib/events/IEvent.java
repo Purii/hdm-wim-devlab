8f9e5c788cbacc585fe460701966eac45babfa64
itsBen <benedikt.zneb@gmail.com>
2017-06-18 18:36:47 +0200

    added default methods to interface

--- SharedLib/src/main/java/de/hdm/wim/sharedLib/events/IEvent.java
+++ SharedLib/src/main/java/de/hdm/wim/sharedLib/events/IEvent.java
@@ -6,8 +6,33 @@ import java.util.Hashtable;
  * Created by ben on 13/06/2017.
  */
 public interface IEvent {
-	String data				= "";
-	String id 				= "";
-	String publishTime 		= "";
-	Hashtable attributes 	= new Hashtable();
+	String data								= "";
+	String id 								= "";
+	String publishTime 						= "";
+	Hashtable<String, String> attributes 	= new Hashtable<String, String>();
+
+	String getData();
+	void setData(String data);
+
+	String getId();
+	void setId(String id);
+
+	String getPublishTime();
+	void setPublishTime(String publishTime);
+
+	String getEventType();
+	//void setEventType(String EventType);
+
+	String getEventSource();
+	void setEventSource(String EventSource);
+
+
+	// not sure if we need this
+	/*
+	String getAttribute(String key);
+	void setAttribute(String key, String value);
+	*/
+
+	Hashtable<String, String> getAttributes();
+	void setAttributes(Hashtable<String, String> attributes);
 }
