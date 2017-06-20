50f304b05e79875fe7342486963cbface25cf542
itsBen <benedikt.zneb@gmail.com>
2017-06-20 20:11:30 +0200

    implement pubsub

--- SpeechTokenizer/src/main/java/rest/Rest.java
+++ SpeechTokenizer/src/main/java/rest/Rest.java
@@ -1,6 +1,9 @@
-
 package rest;
 
+import com.google.gson.Gson;
+import de.hdm.wim.sharedLib.Constants.PubSub.Topic;
+import de.hdm.wim.sharedLib.events.TokenEvent;
+import de.hdm.wim.sharedLib.pubsub.helper.PublishHelper;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -33,31 +36,38 @@ public class Rest {
 	private ElasticsearchCommunication elasticsearchCommunication = new ElasticsearchCommunication();
 	private CustomKeywordFilter customKeywordFilter = new CustomKeywordFilter();
 	private Protocol protocol = new Protocol();
-	
+
 	@GET @Path("test")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Response findByName() throws JSONException, IOException {
 		return Response.status(200).entity("test").build();
 	}
-	
+
+	@POST @Path("pushtest")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response receivePush(String json) throws Exception {
+		return Response.status(200).entity(json).build();
+	}
+
 	@POST @Path("token")
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response receiveToken(Object objToken) throws Exception {		
+	public Response receiveToken(Object objToken) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		ElasticsearchResult elasticsearch = new ElasticsearchResult();
 		ArrayList<String> listKontexte = new ArrayList<String>();
-		
+
 		if(logger.isInfoEnabled()){
 			logger.info("Received Information from GUI: " + objToken);
 		}
-		
+
 		//parse received json to JAVA-String
 		String jsonInString = mapper.writeValueAsString(objToken);
 		if(logger.isInfoEnabled()){
 			logger.info("Parsed GUI-Information to JSON: " + jsonInString);
 		}
-		
+
 		//Die jeweiligen Attribute des erhaltenen JSONS in ein JAVA-Objekt mappen
 		TextInformation token = mapper.readValue(jsonInString, TextInformation.class);
 
@@ -67,48 +77,91 @@ public class Rest {
 
 		listKontexte.add("Videokonferenz");
 		elasticsearch.setListKontexte(listKontexte);
-		
-		if(ElasticsearchCommunication.checkExistingFilter() == true){
-			JSONObject jsonObject = startTokenaziation(elasticsearch, token);
-			return Response.status(200).entity(jsonObject.toString()).build();
-		} else {
-			//creating new Elasticsearch index with the custom filter
-			ElasticsearchCommunication.createCustomFilter();
-			
-			JSONObject jsonObject = startTokenaziation(elasticsearch, token);
 
-			//sendToGooglePubSub(jsonObject);
-			return Response.status(200).entity(jsonObject.toString()).build();
+		PublishHelper ph = new PublishHelper(false);
+
+		try{
+			if(ElasticsearchCommunication.checkExistingFilter() == true){
+				TokenEvent tokenEvent 	= startTokenaziation(elasticsearch, token);
+				String tokenEventString =  new Gson().toJson(tokenEvent);
+
+				ph.Publish(tokenEvent, Topic.TOPIC_1, true);
+
+				return Response.status(200).entity(tokenEventString).build();
+			} else {
+				//creating new Elasticsearch index with the custom filter
+				ElasticsearchCommunication.createCustomFilter();
+
+				TokenEvent tokenEvent 	= startTokenaziation(elasticsearch, token);
+				String tokenEventString =  new Gson().toJson(tokenEvent);
+
+				ph.Publish(tokenEvent, Topic.TOPIC_1, true);
+
+				return Response.status(200).entity(tokenEventString).build();
+			}
+		}
+		catch ( Exception e	){
+			return Response.status(500).entity(e.toString()).build();
+
 		}
 	}
-	
-	public JSONObject startTokenaziation(ElasticsearchResult elasticsearch, TextInformation token) throws Exception{
+
+/*	public JSONObject startTokenaziation(ElasticsearchResult elasticsearch, TextInformation token) throws Exception{
 		JSONObject jsonObject = new JSONObject();
 		GoogleDriveCommunication googleDriveCommunication = new GoogleDriveCommunication();
-		
+
 		KeywordFilter keywordfilter = customKeywordFilter.keywordFilter(token.getTextresultat());
-		
+
 		elasticsearch.setListTokens(elasticsearchCommunication.sendToElasticSearch(keywordfilter.getTokenWithoutKeywords()));
 		ArrayList<String> listTokens = new ArrayList<String>(elasticsearch.getListTokens());
 		listTokens.addAll(keywordfilter.getListFilteredKeywords());
-		
+
 		//creating jsonobject for response
 		try {
 			jsonObject = createJSONObjectForResponse(elasticsearch, listTokens);
 		} catch (Exception err) {
 			logger.error("Could not parse JSONObject:"+err);
 		}
-		
+
 		//creating the protocol
 		String protocolname = protocol.createProtocol(token.getTimestamp(), token.getSessionID(), token.getTextresultat(), token.getUserID());
-		
+
 		//uploading the document to google drive
 		GoogleDriveCommunication.saveProtocolOnGoogleDrive(protocolname);
 		//*TODO Upload Google Drive
 		//googleDriveCommunication.uploadToGoogleDrive();
 		return jsonObject;
 	}
-	
+	*/
+
+	public TokenEvent startTokenaziation(ElasticsearchResult elasticsearch, TextInformation token) throws Exception{
+		TokenEvent tokenevent = new TokenEvent();
+		GoogleDriveCommunication googleDriveCommunication = new GoogleDriveCommunication();
+
+		KeywordFilter keywordfilter = customKeywordFilter.keywordFilter(token.getTextresultat());
+
+		elasticsearch.setListTokens(elasticsearchCommunication.sendToElasticSearch(keywordfilter.getTokenWithoutKeywords()));
+		ArrayList<String> listTokens = new ArrayList<String>(elasticsearch.getListTokens());
+		listTokens.addAll(keywordfilter.getListFilteredKeywords());
+
+		//creating jsonobject for response
+		try {
+			tokenevent = createTokenEventForResponse(elasticsearch, listTokens);
+		} catch (Exception err) {
+			logger.error("Could not parse JSONObject:"+err);
+		}
+
+		//creating the protocol
+		String protocolname = protocol.createProtocol(token.getTimestamp(), token.getSessionID(), token.getTextresultat(), token.getUserID());
+
+		//uploading the document to google drive
+		GoogleDriveCommunication.saveProtocolOnGoogleDrive(protocolname);
+		//*TODO Upload Google Drive
+		//googleDriveCommunication.uploadToGoogleDrive();
+		return tokenevent;
+	}
+
+
 	public JSONObject createJSONObjectForResponse(ElasticsearchResult elasticsearch, ArrayList<String> listTokens) throws JSONException{
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("sessionID", elasticsearch.getSessionID());
@@ -117,5 +170,20 @@ public class Rest {
 		jsonObject.put("kontexts", elasticsearch.getListKontexte());
 		jsonObject.put("tokens", listTokens);
 		return jsonObject;
+	}
+
+	public TokenEvent createTokenEventForResponse(ElasticsearchResult elasticsearch, ArrayList<String> listTokens) throws Exception{
+
+		TokenEvent tokenevent = new TokenEvent();
+
+		tokenevent.setData("test123");
+		tokenevent.setSessionId(elasticsearch.getSessionID());
+		tokenevent.setUserId(elasticsearch.getUserID());
+		//	tokenevent.put("timestamp", elasticsearch.getTimestamp());
+		tokenevent.setContexts(elasticsearch.getListKontexte().toString());
+		tokenevent.setTokens(listTokens.toString());
+
+
+		return tokenevent;
 	}
 }
