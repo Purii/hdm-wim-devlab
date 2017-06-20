cd1213f6ae859ea8e289b9984e8e5dc4bf56f9f6
nilsbch <n.bchmnn@gmail.com>
2017-06-20 12:49:20 +0200

    Updated Patterns

---
+++ EventServices/eventProcessing/src/main/java/de/hdm/wim/eventServices/eventProcessing/cep/patterns/IdentificateProjectContext.java
@@ -0,0 +1,7 @@
+package de.hdm.wim.eventServices.eventProcessing.cep.patterns;
+
+/**
+ * Created by nilsb on 13.06.2017.
+ */
+public class IdentificateProjectContext {
+}
--- eventServices/eventProcessing/src/main/java/de/hdm/wim/eventServices/eventProcessing/RunFlink.java
+++ eventServices/eventProcessing/src/main/java/de/hdm/wim/eventServices/eventProcessing/RunFlink.java
@@ -2,7 +2,9 @@ package de.hdm.wim.eventServices.eventProcessing;
 
 import com.google.gson.GsonBuilder;
 import de.hdm.wim.eventServices.eventProcessing.cep.patterns.TestPattern2;
-import de.hdm.wim.sharedLib.pubsub.classes.PubSubMessage;
+import de.hdm.wim.sharedLib.events.IEvent;
+import de.hdm.wim.sharedLib.helper.Helper;
+import com.google.pubsub.v1.PubsubMessage;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -50,12 +52,12 @@ public class RunFlink {
             relationToDatePattern.run(env, tokenStream);
 */
 
-			DataStream<PubSubMessage> messageStream = env
+			DataStream<PubsubMessage> messageStream = env
 				.socketTextStream("localhost", 9999)
 				.flatMap(new Splitter());
 
-			TestPattern2 testPattern2 = new TestPattern2();
-			testPattern2.run(env, messageStream);
+	//		TestPattern2 testPattern2 = new TestPattern2();
+	//		testPattern2.run(env, messageStream);
 
             // print message stream
 
@@ -63,12 +65,13 @@ public class RunFlink {
 
             env.execute("CEP chat stream job");
 		}
-	public static class Splitter implements FlatMapFunction<String, PubSubMessage> {
+	public static class Splitter implements FlatMapFunction<String, PubsubMessage> {
 		@Override
-		public void flatMap(String value, Collector<PubSubMessage> out) throws Exception {
-			PubSubMessage psm = new GsonBuilder().create().fromJson(value, PubSubMessage.class);
-			out.collect(new PubSubMessage(psm.getData(),psm.getMessageId(), psm.getPublishTime(), psm.getAttributes()));
-
+		public void flatMap(String value, Collector<PubsubMessage> out) throws Exception {
+			PubsubMessage psm = new GsonBuilder().create().fromJson(value, PubsubMessage.class);
+			Helper helper = new Helper();
+			//out.collect(new PubsubMessage(psm.getData(),psm.getMessageId(), psm.getPublishTime(), psm.getAttributesMap()));
 		}
 	}
+
 }
