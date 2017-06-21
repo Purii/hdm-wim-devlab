e9d439f599fc456f623272a29d8927811405a15c
itsBen <benedikt.zneb@gmail.com>
2017-06-21 23:55:01 +0200

    updated constants

--- sharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
+++ sharedLib/src/main/java/de/hdm/wim/sharedLib/Constants.java
@@ -250,5 +250,6 @@ public class Constants {
 		public static final String ATTRIBUTE_VALUE 	= "value";
 		public static final String PAYLOAD 			= "payload";
 		public static final String SECRET_TOKEN 	= "secretToken";
+		public static final String JSON_INPUT		= "json-input";
 	}
 }
