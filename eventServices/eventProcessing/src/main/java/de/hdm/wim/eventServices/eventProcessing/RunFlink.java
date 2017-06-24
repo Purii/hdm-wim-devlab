f6915ad19814e718c9980ce152e8663f8ba83342
Patrick Puritscher <blackout289@gmail.com>
2017-06-24 13:49:10 +0200

    complete EventSplitter

--- eventServices/eventProcessing/src/main/java/de/hdm/wim/eventServices/eventProcessing/RunFlink.java
+++ eventServices/eventProcessing/src/main/java/de/hdm/wim/eventServices/eventProcessing/RunFlink.java
@@ -4,10 +4,10 @@ import com.google.gson.GsonBuilder;
 import de.hdm.wim.eventServices.eventProcessing.cep.patterns.DocumentContextPattern;
 import de.hdm.wim.eventServices.eventProcessing.cep.patterns.HighlyRelevantDocumentPattern;
 import de.hdm.wim.sharedLib.Constants;
-import de.hdm.wim.sharedLib.events.IEvent;
-import de.hdm.wim.sharedLib.events.StayAliveEvent;
+import de.hdm.wim.sharedLib.events.*;
 import de.hdm.wim.sharedLib.helper.Helper;
 import com.google.pubsub.v1.PubsubMessage;
+import jdk.nashorn.internal.parser.Token;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -131,7 +131,64 @@ public class RunFlink {
 			PubsubMessage msg = new GsonBuilder().create().fromJson(value, PubsubMessage.class);
 			Helper helper = new Helper();
 			IEvent evt = helper.convertToIEvent(msg);
-			out.collect(evt);
+
+			switch(evt.getEventType()) {
+				case Constants.PubSub.EventType.ADDITIONAL_USER_INFO:
+					out.collect((AdditionalUserInformationEvent) evt);
+				case Constants.PubSub.EventType.ALL_COMPANIES:
+					out.collect((AllCompaniesEvent) evt);
+				case Constants.PubSub.EventType.ALL_DEPARTMENTS:
+					out.collect((AllDepartmentsEvent) evt);
+				case Constants.PubSub.EventType.ALL_PROJECTROLES:
+					out.collect((AllProjectRolesEvent) evt);
+				case Constants.PubSub.EventType.ALL_PROJECTS:
+					out.collect((AllProjectsEvent) evt);
+				case Constants.PubSub.EventType.DEPARTMENT_INFO:
+					out.collect((DepartmentInformationEvent) evt);
+				case Constants.PubSub.EventType.DOCUMENT_CALL:
+					out.collect((DocumentCallEvent) evt);
+				case Constants.PubSub.EventType.DOCUMENT_CONTEXT:
+					out.collect((DocumentCallEvent) evt);
+				case Constants.PubSub.EventType.DOCUMENT_HIGHLY_RELEVANT:
+					out.collect((DocumentHighlyRelevantEvent) evt);
+				case Constants.PubSub.EventType.DOCUMENT_INFO:
+					out.collect((DocumentInformationEvent) evt);
+				case Constants.PubSub.EventType.FEEDBACK:
+					out.collect((FeedbackEvent) evt);
+				case Constants.PubSub.EventType.INFO_TOALL_DOCUMENTS:
+					out.collect((InformationToAllDocumentsEvent) evt);
+				case Constants.PubSub.EventType.LEARN:
+					out.collect((LearnEvent) evt);
+				case Constants.PubSub.EventType.OFFER:
+					out.collect((OfferEvent) evt);
+				case Constants.PubSub.EventType.PROJECT_INFO:
+					out.collect((ProjectInformationEvent) evt);
+				case Constants.PubSub.EventType.SESSION_END:
+					out.collect((SessionEndEvent) evt);
+				case Constants.PubSub.EventType.SESSION_START:
+					out.collect((SessionStartEvent) evt);
+				case Constants.PubSub.EventType.STAYALIVE:
+					out.collect((StayAliveEvent) evt);
+				case Constants.PubSub.EventType.SUCCESSFUL_FEEDBACK:
+					out.collect((SuccessfulFeedbackEvent) evt);
+				case Constants.PubSub.EventType.TOKEN:
+					out.collect((TokenEvent) evt);
+				case Constants.PubSub.EventType.USER_CONTEXT:
+					out.collect((UserContextEvent) evt);
+				case Constants.PubSub.EventType.USER_INACTIVE:
+					out.collect((UserInactiveEvent) evt);
+				case Constants.PubSub.EventType.USER_INFO:
+					out.collect((UserInformationEvent) evt);
+				case Constants.PubSub.EventType.USER_JOINED_SESSION:
+					out.collect((UserJoinedSessionEvent) evt);
+				case Constants.PubSub.EventType.USER_LEFT_SESSION:
+					out.collect((UserLeftSessionEvent) evt);
+				case Constants.PubSub.EventType.USER_LOGIN:
+					out.collect((UserLoginEvent) evt);
+				case Constants.PubSub.EventType.USER_START:
+					out.collect((UserStartEvent) evt);
+				default: out.collect(evt);
+			}
 		}
 	}
 	public static class StayAliveEventSplitter implements FlatMapFunction<String, StayAliveEvent> {
