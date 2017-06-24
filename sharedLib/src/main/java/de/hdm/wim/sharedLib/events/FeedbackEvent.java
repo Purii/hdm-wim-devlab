c5f652acba1daa5ee33f10c5101d0180b4e64883
itsBen <benedikt.zneb@gmail.com>
2017-06-24 12:59:32 +0200

    test

--- sharedLib/src/main/java/de/hdm/wim/sharedLib/events/FeedbackEvent.java
+++ sharedLib/src/main/java/de/hdm/wim/sharedLib/events/FeedbackEvent.java
@@ -8,7 +8,7 @@ import java.util.Map;
 /**
  * Created by GezimKrasniqi on 20.06.17.
  */
-public class FeedbackEvent implements IEvent{
+public class FeedbackEvent extends IEvent{
 
 	private String data;
 	private String id;
@@ -17,56 +17,6 @@ public class FeedbackEvent implements IEvent{
 
 	public FeedbackEvent(){
 		this.attributes.put(AttributeKey.EVENT_TYPE, EventType.FEEDBACK);
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id= id;
-	}
-
-	public String getPublishTime() {
-		return publishTime;
-	}
-
-	public void setPublishTime(String publishTime) {
-		this.publishTime = publishTime;
-	}
-
-	public String getEventType() {
-		return attributes.get(AttributeKey.EVENT_TYPE).toString();
-	}
-
-	/*
-	public void setEventType(String EventType){
-		this.attributes.put(AttributeKey.EVENT_TYPE, EventType);
-	}
-	*/
-
-	public String getEventSource() {
-		return attributes.get(AttributeKey.EVENT_SOURCE).toString();
-	}
-
-	public void setEventSource(String EventSource) {
-		this.attributes.put(AttributeKey.EVENT_SOURCE, EventSource);
-	}
-
-	public Map<String, String> getAttributes() {
-		return this.attributes;
-	}
-
-	public void setAttributes(Map<String, String> attributes) {
-		this.attributes = attributes;
 	}
 
 	public String getUserId(){
