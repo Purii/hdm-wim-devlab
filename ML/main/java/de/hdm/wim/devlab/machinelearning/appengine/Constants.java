57e39560bd7f94ad9962ae1ff37d546434788aa1
LepiorzDaniel <dl047@hdm-stuttgart.de>
2017-06-26 15:26:32 +0200

    Konstanten-Klasse - Appengine

---
+++ ML/main/java/de/hdm/wim/devlab/machinelearning/appengine/Constants.java
@@ -0,0 +1,21 @@
+package de.hdm.wim.devlab.machinelearning.appengine;
+
+/**
+ * Konstantenklasse.
+ */
+public final class Constants {
+	/**
+	 * Definition des Memcache-Key zur Zwischenspeicherung von Nachrichten. 
+	 */
+	public static final String MESSAGE_CACHE_KEY = "devlabml12345";
+	/**
+	 * Das Baispaket
+	 */
+	public static final String BASE_PACKAGE = "de.hdm.wim.devlab.machinelearning";
+
+	/**
+	 * Konstanten-Konstruktur.
+	 */
+	private Constants() {
+	}
+}
